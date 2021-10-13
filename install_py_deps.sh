766750ea5800ce7423790b84e221d15cea1723cd
h3xept <h3xept@gmail.com>
2021-10-13 16:19:08 +0200

    Improved install script

--- install_py_deps.sh
+++ install_py_deps.sh
@@ -1,16 +1,20 @@
 DEPENDENCIES_FOLDER=$1
 
+
 PROBE_SYSTEM="${DEPENDENCIES_FOLDER}/CAELUS_ProbeSystem";
 THERMAL_MODEL="${DEPENDENCIES_FOLDER}/CAELUS_ThermalModel";
 SMARTSKIES="${DEPENDENCIES_FOLDER}/CAELUS_SmartSkies";
 POWER_MODELS="${DEPENDENCIES_FOLDER}/CAELUS_PowerModels";
+PYTHON_DEPS=($PROBE_SYSTEM $THERMAL_MODEL $SMARTSKIES $POWER_MODELS)
 
 echo "[🚚] Installing Python Dependencies";
-echo "- Installing Probe System"
-pip3 install $PROBE_SYSTEM --upgrade
-echo "- Installing Thermal Model"
-pip3 install $THERMAL_MODEL --upgrade
-echo "- Installing Smartskies SDK"
-pip3 install $SMARTSKIES --upgrade
-echo "- Installing Power Models"
-pip3 install $POWER_MODELS --upgrade
+for DEP in $PYTHON_DEPS
+do 
+echo "- Installing ${DEP##*/}"
+pip3 install $DEP --upgrade
+echo "- Installing ${DEP##*/}'s Dependencies"
+pip3 install -r "${DEP}/requirements.txt"
+done
+
+echo "[🚚] Installing DigitalTwin Python Dependencies"
+pip3 install -r "requirements.txt"
