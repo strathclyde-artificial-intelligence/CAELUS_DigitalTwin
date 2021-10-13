5f2d3b4a966f3c87e30520496e146cc41e004e21
h3xept <h3xept@gmail.com>
2021-10-13 17:26:26 +0200

    Fixed install script

---
+++ DigitalTwin/src/SimulationStack.py
@@ -0,0 +1,7 @@
+
+
+class SimulationStack():
+    def __init__(self):
+        pass
+
+    
--- install_py_deps.sh
+++ install_py_deps.sh
@@ -5,15 +5,15 @@ PROBE_SYSTEM="${DEPENDENCIES_FOLDER}/CAELUS_ProbeSystem";
 THERMAL_MODEL="${DEPENDENCIES_FOLDER}/CAELUS_ThermalModel";
 SMARTSKIES="${DEPENDENCIES_FOLDER}/CAELUS_SmartSkies";
 POWER_MODELS="${DEPENDENCIES_FOLDER}/CAELUS_PowerModels";
-PYTHON_DEPS=($PROBE_SYSTEM $THERMAL_MODEL $SMARTSKIES $POWER_MODELS)
+declare -a PYTHON_DEPS=($PROBE_SYSTEM $THERMAL_MODEL $SMARTSKIES $POWER_MODELS)
 
 echo "[🚚] Installing Python Dependencies";
-for DEP in $PYTHON_DEPS
+for DEP in ${PYTHON_DEPS[@]};
 do 
-echo "- Installing ${DEP##*/}"
-pip3 install $DEP --upgrade
-echo "- Installing ${DEP##*/}'s Dependencies"
-pip3 install -r "${DEP}/requirements.txt"
+    echo "- Installing ${DEP##*/}"
+    pip3 install $DEP --upgrade
+    echo "- Installing ${DEP##*/}'s Dependencies"
+    pip3 install -r "${DEP}/requirements.txt"
 done
 
 echo "[🚚] Installing DigitalTwin Python Dependencies"
