752ac886ac4b4de2e3792461bffb683c4138c2d5
h3xept <h3xept@gmail.com>
2021-10-15 18:35:48 +0200

    Removed sim

--- 6-dof-simulator/src/Interfaces/DroneController.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DRONECONTROLLER_H__
-#define __DRONECONTROLLER_H__
-
-#include "../Interfaces/AsyncDroneControl.h"
-#include "../Interfaces/TimeHandler.h"
-
-class DroneController : public AsyncDroneControl, public TimeHandler {};
-
-#endif // __DRONECONTROLLER_H__
