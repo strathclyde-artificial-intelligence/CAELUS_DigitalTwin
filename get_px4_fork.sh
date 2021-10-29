933611822c28b6025e4ba6c929a79a9b78d035cf
h3xept <h3xept@gmail.com>
2021-10-29 15:01:38 +0200

    Added some instructions

--- get_px4_fork.sh
+++ get_px4_fork.sh
@@ -1,3 +1,5 @@
+RED='\033[0;31m'
+NC='\033[0m'
 PX4_DIR=./Dependencies/PX4-Autopilot
 read -e -p "Insert the folder containing the CAELUS fork for px4: (default $PX4_DIR [downloaded from repo if not present])" input
 PX4_DIR="${input:-$PX4_DIR}"
@@ -6,10 +8,10 @@ echo $PX4_DIR
 echo "[🍴] Fethcing CAELUS fork for PX4-Autopilot"
 if [ ! -d $PX4_DIR ]; then
     git clone https://github.com/strathclyde-artificial-intelligence/PX4-Autopilot $PX4_DIR --recursive
-    (cd $PX4_DIR; make)
 fi
 
 echo "[🚚] Installing PX4-Autpilot python dependencies"
 pip3 install -r px4_python_requirements.txt
+(cd $PX4_DIR; make)
 
 echo -e "Make sure to issue `${RED}export PX4_ROOT_FOLDER=$PX4_DIR${NC}` before starting the digital twin."
