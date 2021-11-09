88e3d416e5092d4ab411f0c64118091a5e9ecb05
h3xept <h3xept@gmail.com>
2021-11-09 13:48:02 +0100

    Interpolated sensor time for battery integration

--- Dependencies/CAELUS_PowerModels/PowerModels/PowerTrain/Battery.py
+++ Dependencies/CAELUS_PowerModels/PowerModels/PowerTrain/Battery.py
@@ -4,35 +4,36 @@ from .power_train_esc_motor import powertrain_ESC_Motor
 from time import time
 
 class Battery():
-    def __init__(self, initial_voltage, initial_modulation_idx, timestep_h, motors_n = 4):
+    def __init__(self, initial_voltage, initial_modulation_idx, motors_n = 4):
         self.__current_modulation_idxs = [initial_modulation_idx]*motors_n
         self.__current_voltage = initial_voltage
         self.__internal_time = 0
         self.__depth_of_discharge = 0
-        self.__timestep_h = timestep_h
         self.__motors_n = motors_n
+        self.__last_control = [0] * motors_n
 
-    def new_control(self, controls: List[float]) -> Tuple[float, float]:
+    def new_control(self, controls: List[float], dt_hr: float) -> Tuple[float, float]:
         controls = [max(v, 0) for v in controls]
+
         # HOTFIX FOR FUNCTION THAT HANGS -- MAZHE SHOULD PROVIDE A FIX
         # DELETE WHEN FIXED!!
-        controls = [c if c > 0.2 else 0 for c in controls]
+        controls = [c if c > 0.1 else 0 for c in controls]
         ##
         mod_idxs, capacities_extracted, current_demands = [m for m in self.__current_modulation_idxs], [], []
         for i in range(self.__motors_n):
-            motor_pwm = controls[i]
-            if motor_pwm == 0:
-                continue
-            _, _, new_mod_idx, capacity_extracted, current_demand = powertrain_ESC_Motor(
-                    controls[i],
-                    self.__current_modulation_idxs[i],
-                    self.__current_voltage,
-                    self.__timestep_h)
+            motor_pwm = self.__last_control[i]
+            if motor_pwm != 0:
+                _, _, new_mod_idx, capacity_extracted, current_demand = powertrain_ESC_Motor(
+                        self.__last_control[i],
+                        self.__current_modulation_idxs[i],
+                        self.__current_voltage,
+                        dt_hr)
 
-            mod_idxs.append(new_mod_idx)
-            capacities_extracted.append(capacity_extracted)
-            current_demands.append(current_demand)
-            
+                mod_idxs[i] = new_mod_idx
+                capacities_extracted.append(capacity_extracted)
+                current_demands.append(current_demand)
+                
+            self.__last_control[i] = controls[i]
 
         new_discharge, new_voltage = batt_disc(self.__depth_of_discharge, sum(capacities_extracted), sum(current_demands))
 
@@ -40,7 +41,7 @@ class Battery():
         self.__current_voltage = new_voltage
         self.__depth_of_discharge = new_discharge
 
-        self.__internal_time += self.__timestep_h
+        self.__internal_time += dt_hr
 
         return self.__current_voltage, self.__depth_of_discharge
 
--- DigitalTwin/Probes/QuadrotorBatteryDischarge.py
+++ DigitalTwin/Probes/QuadrotorBatteryDischarge.py
@@ -4,21 +4,31 @@ from PowerModels.PowerTrain.Battery import Battery
 from ..Vehicle import Vehicle
 from time import time
 
+US_TO_HR = 1 / 3.6e9
+
 class QuadrotorBatteryDischarge(Subscriber):
     
-    def __init__(self, integration_timestep=1.11111e-6):
-        self.__battery = Battery(25.2, 0.0, integration_timestep)
+    def __init__(self):
+        self.__battery = Battery(25.2, 0.0)
         self.__vehicle = None
+        self.__last_timestamp = 0
 
     def set_vehicle(self, vehicle):
         self.__vehicle: Vehicle = vehicle
 
     def new_datapoint(self, drone_id, stream_id, datapoint):
+
         if self.__vehicle is None:
             return
         if self.__battery.get_battery_level() == 0:
             return
-        curr_voltage, depth_of_discharge = self.__battery.new_control(datapoint.controls)
+        
+        # UORb messages are not guaranteed to arrive once per timestep - we must interpolate integration steps
+        ts = datapoint.time_boot_us 
+        dt = datapoint.time_boot_us - self.__last_timestamp
+        self.__last_timestamp = ts
+
+        curr_voltage, depth_of_discharge = self.__battery.new_control(datapoint.controls, dt * US_TO_HR)
         self.__vehicle.message_factory.battery_status_send(
             0, # batt-id
             1, # battery function https://mavlink.io/en/messages/common.html#MAV_BATTERY_FUNCTION
