e30dda1600d261e53144338aff73d698834d3a26
H3xept <h3xept@gmail.com>
2021-11-22 14:06:13 +0100

    git subrepo pull Dependencies/CAELUS_PowerModels
    subrepo:
      subdir:   "Dependencies/CAELUS_PowerModels"
      merged:   "9ae9a796fd"
    upstream:
      origin:   "https://github.com/strathclyde-artificial-intelligence/CAELUS_PowerModels"
      branch:   "main"
      commit:   "9ae9a796fd"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- Dependencies/CAELUS_PowerModels/PowerModels/PowerTrain/Battery.py
+++ Dependencies/CAELUS_PowerModels/PowerModels/PowerTrain/Battery.py
@@ -10,7 +10,6 @@ class Battery():
         self.__internal_time = 0
         self.__depth_of_discharge = 0
         self.__motors_n = motors_n
-        self.__last_control = [0] * motors_n
 
     def new_control(self, controls: List[float], dt_hr: float) -> Tuple[float, float]:
         controls = [max(v, 0) for v in controls]
@@ -21,19 +20,15 @@ class Battery():
         ##
         mod_idxs, capacities_extracted, current_demands = [m for m in self.__current_modulation_idxs], [], []
         for i in range(self.__motors_n):
-            motor_pwm = self.__last_control[i]
-            if motor_pwm != 0:
-                _, _, new_mod_idx, capacity_extracted, current_demand = powertrain_ESC_Motor(
-                        self.__last_control[i],
-                        self.__current_modulation_idxs[i],
-                        self.__current_voltage,
-                        dt_hr)
+            _, _, new_mod_idx, capacity_extracted, current_demand = powertrain_ESC_Motor(
+                    controls[i],
+                    self.__current_modulation_idxs[i],
+                    self.__current_voltage,
+                    dt_hr)
 
-                mod_idxs[i] = new_mod_idx
-                capacities_extracted.append(capacity_extracted)
-                current_demands.append(current_demand)
-                
-            self.__last_control[i] = controls[i]
+            mod_idxs[i] = new_mod_idx
+            capacities_extracted.append(capacity_extracted)
+            current_demands.append(current_demand)
 
         new_discharge, new_voltage = batt_disc(self.__depth_of_discharge, sum(capacities_extracted), sum(current_demands))
 
---
+++ Dependencies/CAELUS_PowerModels/tests/test_battery.py
@@ -0,0 +1,7 @@
+from PowerModels.PowerTrain.Battery import Battery
+
+def test_battery():
+    controls = [0.2, 0.2, 0.2, 0.2]
+    batt = Battery(25.0, 0.3)
+    voltage, dod = batt.new_control(controls, 1 / 3600) # List of [0-1] values [0.00001, ..., 0.999999]
+    print(voltage, dod)
