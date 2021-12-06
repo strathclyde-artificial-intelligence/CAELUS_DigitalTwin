a5ab7d9b21acade711998d63aa3452798d40e9f8
H3xept <h3xept@gmail.com>
2021-12-06 13:25:10 +0100

    JMAVSim is now started standalone

--- DigitalTwin/SimulationController.py
+++ DigitalTwin/SimulationController.py
@@ -3,6 +3,7 @@ import os
 from .Interfaces.Stoppable import Stoppable
 from .Wrappers.PX4Wrapper import PX4Wrapper
 from .Wrappers.SimulatorWrapper import SimulatorWrapper
+from .Wrappers.JMAVSimWrapper import JMAVSimWrapper
 
 class SimulationController(Stoppable):
     
@@ -18,8 +19,8 @@ class SimulationController(Stoppable):
     def __initialise_all(self):
         if self.__px4_wrapper is None:
             self.__initialise_px4(self.__stream_handler)
-        # if self.__simulator_wrapper is None:
-        #     self.__initialise_simulator(self.__stream_handler)
+        if self.__simulator_wrapper is None:
+            self.__initialise_simulator(self.__stream_handler)
 
     def __initialise_px4(self, stream_handler):
         if SimulationController.PX4_FOLDER_ENVIRON not in os.environ:
@@ -34,22 +35,28 @@ class SimulationController(Stoppable):
 
     def __initialise_simulator(self, stream_handler):
         self.__logger.info('Initialising simulator instance...')
-        self.__simulator_wrapper = SimulatorWrapper(f"{'/'.join(os.path.dirname(__file__).split('/')[:-1])}/Dependencies/Simulator/build/", stream_handler=stream_handler)
+        # Strathclyde sim workflow
+        # self.__simulator_wrapper = SimulatorWrapper(f"{'/'.join(os.path.dirname(__file__).split('/')[:-1])}/Dependencies/Simulator/build/", stream_handler=stream_handler)
+        self.__simulator_wrapper = JMAVSimWrapper(
+            f'{os.environ[SimulationController.PX4_FOLDER_ENVIRON]}/Tools/jMAVSim/out/production/',
+            self.__initial_lon_lat_alt,
+            stream_handler=stream_handler
+        )
 
     def start(self):
         self.__initialise_all()
         self.__logger.info('Starting PX4Wrapper Thread')
         self.__px4_wrapper.start()
-        # self.__logger.info('Starting Simulator Thread')
-        # self.__simulator_wrapper.start()
+        self.__logger.info('Starting Simulator Thread')
+        self.__simulator_wrapper.start()
 
     def graceful_stop(self):
         self.__px4_wrapper.graceful_stop()
-        # self.__simulator_wrapper.graceful_stop()
+        self.__simulator_wrapper.graceful_stop()
 
     def halt(self):
         self.__px4_wrapper.halt()
-        # self.__simulator_wrapper.halt()
+        self.__simulator_wrapper.halt()
     
     def reset(self):
         res = True
@@ -58,16 +65,16 @@ class SimulationController(Stoppable):
         if self.__px4_wrapper is not None:
             self.__px4_wrapper.graceful_stop()
             res = self.__px4_wrapper.termination_complete.acquire(timeout=2)
-        # if self.__simulator_wrapper is not None:
-        #     self.__simulator_wrapper.graceful_stop()
-        #     res = res and self.__simulator_wrapper.termination_complete.acquire(timeout=2)
+        if self.__simulator_wrapper is not None:
+            self.__simulator_wrapper.graceful_stop()
+            res = res and self.__simulator_wrapper.termination_complete.acquire(timeout=2)
 
         
         # Wait for locks to acquire - if any fail occurs -- halt thread
         if not res:
             self.__logger.warn('Lock acquisition failed -- Halting threads.')
             self.__px4_wrapper.halt()
-            # self.__simulator_wrapper.halt()
+            self.__simulator_wrapper.halt()
 
         self.__px4_wrapper = None
         self.__simulator_wrapper = None
--- DigitalTwin/Wrappers/JMAVSimWrapper.py
+++ DigitalTwin/Wrappers/JMAVSimWrapper.py
@@ -1,46 +1,37 @@
-from sys import stdin
 import threading
 import subprocess
 import logging
 import signal
-from typing import Optional
 from time import sleep
+from typing import Optional
 from ..Interfaces.Stoppable import Stoppable
 from ..Interfaces.StreamHandler import StreamHandler
 
-class PX4Wrapper(threading.Thread):
+class JMAVSimWrapper(threading.Thread):
 
-    def __init__(self, px4_root_folder_location, initial_lon_lat_alt, stream_handler: Optional[StreamHandler] = None, logger=logging.getLogger(__name__)):
+    def __init__(self, simulator_jar_file_location, initial_lon_lat_alt, stream_handler: Optional[StreamHandler] = None, logger=logging.getLogger(__name__)):
         super().__init__()
-        self.name = 'PX4Wrapper'
-        self.__should_stop = False
+        self.name = 'JMAVSimWrapper'
         self.__initial_lon_lat_alt = initial_lon_lat_alt
-        self.__px4_folder = px4_root_folder_location
+        self.__should_stop = False
+        self.__sim_folder = simulator_jar_file_location
         self.__process = None
         self.__logger = logger
         self.__stream_handler = stream_handler
-        self.termination_complete = threading.Condition()
         self.__streams = set()
+        # Wait for this lock to properly destroy this wrapper
+        self.termination_complete = threading.Condition()
         self.daemon = False
 
-    def __new_stream_available(self, stream_name, stream):
-        if self.__stream_handler is not None:
-            self.__streams.add(stream_name)
-            self.__stream_handler.new_stream_available(stream_name, stream)
-
-    def __invalidate_stream(self, stream_name):
-        if self.__stream_handler is not None:
-            self.__stream_handler.invalidate_stream(stream_name)
-
     def __cleanup(self, timeout = 1):
-        self.__logger.info('Cleaning up resources for PX4 Wrapper')
+        self.__logger.info('Cleaning up resources for Simulator Wrapper')
         
         self.__logger.info(f'Invalidating streams for {__name__}')
         for s in self.__streams:
             self.__invalidate_stream(s)
         self.__streams = []
 
-        self.__logger.info(f'Waiting for PX4 process to exit...')
+        self.__logger.info(f'Waiting for Simulator process to exit...')
         if self.__process is not None:
             for t in range(timeout):
                 code = self.__process.poll()
@@ -51,16 +42,29 @@ class PX4Wrapper(threading.Thread):
             self.__logger.info(f'Poll timeout expired, killing {self.__process}')    
             self.__process.kill()
 
+    def __new_stream_available(self, stream_name, stream):
+        self.__streams.add(stream_name)
+        self.__stream_handler.new_stream_available(stream_name, stream)
+
+    def __invalidate_stream(self, stream_name):
+        self.__stream_handler.invalidate_stream(stream_name)
+
     def run(self):
         self.termination_complete.acquire()
         try:
+            lon, lat, alt = self.__initial_lon_lat_alt
+
             self.__process = subprocess.Popen(
-                'make px4_sitl none_iris',
-                cwd=self.__px4_folder,
+                'export PX4_SIM_SPEED_FACTOR=5; '
+                f'export PX4_HOME_LAT={lat};'
+                f'export PX4_HOME_LON={lon};'
+                f'export PX4_HOME_ALT={alt};'
+                'java -XX:GCTimeRatio=20 -Djava.ext.dirs= -jar jmavsim_run.jar -tcp 127.0.0.1:4560 -r 250 -lockstep -no-gui',
+                cwd=self.__sim_folder,
                 shell=True,
                 stdout=subprocess.PIPE
             )
-            self.__new_stream_available('px4_stdout', self.__process.stdout)
+            self.__new_stream_available('sim_stdout', self.__process.stdout)
             while not self.__should_stop and self.__process.poll() is None:
                 sleep(1)
         except Exception as e:
@@ -71,7 +75,7 @@ class PX4Wrapper(threading.Thread):
             self.termination_complete.release()
 
     def graceful_stop(self):
-        self.__should_stop = True  
+        self.__should_stop = True
 
     def halt(self):
         exit(-1)
