c0c74e9fa6603178baeb6e0aa785f84b859444e7
H3xept <h3xept@gmail.com>
2022-01-12 17:26:52 +0100

    Added mission import statement£

--- DigitalTwin/Wrappers/JMAVSimWrapper.py
+++ DigitalTwin/Wrappers/JMAVSimWrapper.py
@@ -7,6 +7,7 @@ from typing import Optional
 from ..Interfaces.Stoppable import Stoppable
 from ..Interfaces.StreamHandler import StreamHandler
 from ..PayloadModels import SimulatorPayload
+import json
 
 class JMAVSimWrapper(threading.Thread):
 
