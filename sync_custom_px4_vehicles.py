0c89b129ed2fa25eb92a5303ea2ca739f73316bf
H3xept <h3xept@gmail.com>
2022-02-08 10:30:12 +0100

    Added aerodynamics to fw_large

--- sync_custom_px4_vehicles.py
+++ sync_custom_px4_vehicles.py
@@ -35,7 +35,7 @@ def generate_airframe_contents(vehicle_obj):
             f"# @name {name}",
             f"# @type {drone_type}",
             ". ${R}etc/init.d/rc.mc_defaults" if drone_type == "QUADCOPTER" else ". ${R}etc/init.d/rc.vtol_defaults",
-            ". ${R}etc/init.d-posix/airframes/4200_custom_quad" if drone_type == "QUADCOPTER" else ". ${R}etc/init.d-posix/airframes/4200_custom_evtol"
+            ". ${R}etc/init.d-posix/airframes/4200_custom_quad" if drone_type == "QUADCOPTER" else ". ${R}etc/init.d-posix/airframes/4201_custom_evtol"
         ]
 
     def format_parameters(params):
