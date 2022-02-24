f5dc826d11ded7723782ec0e1140175f406653d0
H3xept <h3xept@gmail.com>
2022-02-24 13:58:48 +0100

    Added submit time to delivery object

--- Dependencies/CAELUS_SmartSkies/PySmartSkies/Models/Delivery.py
+++ Dependencies/CAELUS_SmartSkies/PySmartSkies/Models/Delivery.py
@@ -8,7 +8,7 @@ class Delivery(JSONDeserialiser):
     
     def get_object_properties(self):
         return [
-            'id', 'delivery_status', 'vendor_id', 'operator_id', 'customer_id', 'pickup_coordinate', 'dropoff_coordinate', 'pickup_time', 'dropoff_time', 'operation_id'
+            'id', 'delivery_status', 'vendor_id', 'operator_id', 'customer_id', 'pickup_coordinate', 'dropoff_coordinate', 'pickup_time', 'dropoff_time', 'operation_id', 'submit_time'
         ]
 
     def __repr__(self):
